7451ff654dc6590b424a8dcbec8b074a18f81348
Wahbi <wahbi.raihan@ontariotechu.net>
2022-04-24 07:38:44 -0400

    swapped nav for bootstrap navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,79 +1,94 @@
 <template>
-    <nav>
-        <div class="menu-item"><router-link to="/">Home</router-link></div>
+  <nav class="navbar">
+    <div class="container-fluid">
+      <!-- image -->
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <div class="menu-item"><router-link to="/">Home</router-link></div>
+          <div class="menu-item">
+            <router-link to="/settings">Settings</router-link>
+          </div>
+          <NavDropdown />
+          <UserDropdown class="menu-item" id="signin" v-if="store.state.isLoggedIn"/>
+          <div class="signin" id="signin" v-else>
+            <router-link to="/sign-in">Sign-in</router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <!-- <div class="menu-item"><router-link to="/">Home</router-link></div>
         <div class="menu-item"><router-link to="/settings">Settings</router-link></div>
         <NavDropdown /> 
         <UserDropdown class="menu-item" id="signin" v-if="store.state.isLoggedIn" />
         <div class="signin" id="signin" v-else><router-link to="/sign-in">Sign-in</router-link></div>
-    </nav>
-    <router-view/>
+    </nav> -->
+  <router-view />
 </template>
 
 <script>
-    import NavDropdown from './NavbarDropdown.vue';
-    import UserDropdown from '../components/UserDropdown.vue';
-    import { useStore } from 'vuex'
-    //import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-    export default{
-        
-        name: 'AppNavbar',
-        components:{
-            NavDropdown,
-            UserDropdown
-        },
-        data() {
+import NavDropdown from "./NavbarDropdown.vue";
+import UserDropdown from "../components/UserDropdown.vue";
+import { useStore } from "vuex";
+//import firebase from 'firebase/compat/app';
+import "firebase/compat/auth";
+export default {
+  name: "AppNavbar",
+  components: {
+    NavDropdown,
+    UserDropdown,
+  },
+  data() {
     return {
-        store: useStore(),
-        isLoggedIn: false
-    };},
-        computed: {
-            checkLogin() {
-                return this.$store.state.isLoggedIn;
-            }
-        }
-    }
-    
+      store: useStore(),
+      isLoggedIn: false,
+    };
+  },
+  computed: {
+    checkLogin() {
+      return this.$store.state.isLoggedIn;
+    },
+  },
+};
 </script>
 
 <style scoped>
-    nav {
-        background-color: #171717;
-        color: #EDEDED;
-        font-size: 3.5vw;
-        overflow: hidden;
-        font-weight: bold;
-    }
+nav {
+  background-color: #171717;
+  color: #ededed;
+  font-size: 3.5vw;
+  overflow: hidden;
+  font-weight: bold;
+  
+}
 
-    /* Media query to limit max font-size */
-    @media screen and (min-width: 600px) {
-        nav {
-            font-size: 26px;
-        }
-    }
+/* Media query to limit max font-size */
+@media screen and (min-width: 600px) {
+  nav {
+    font-size: 26px;
+  }
+}
 
-    /* Media query to center navbar elements on small screens*/
-    @media screen and (max-width: 600px) {
-        nav {
-            display: flex;
-            justify-content: center;
-        }
-    }
-    
-    div{
-        float: left;
-        display: block;
-        text-align: center;
-        padding: 15px 15px 0px 15px;
-    }
+/* Media query to center navbar elements on small screens*/
+@media screen and (max-width: 600px) {
+  nav {
+    display: flex;
+    justify-content: center;
+  }
+}
 
-    a {
-        color: #EDEDED;
-        text-decoration: none;
-    }
-    #signin{
-        align-content: center;
-        justify-content: center;
-    }
+div {
+  float: left;
+  display: block;
+  text-align: center;
+  padding: 0.25rem 1rem 0px 0.25rem;
+}
+
+a {
+  color: #ededed;
+  text-decoration: none;
+}
+
+
 
 </style>
--- src/components/UserDropdown.vue
+++ src/components/UserDropdown.vue
@@ -1,93 +1,99 @@
 <template>
-    <div class="dropdown">
-        <button class="dropbtn"><img id="profilePic2">{{name}} ▾</button>
-        <div class="dropmenu" id="dropmenu">
-            <div><router-link to="/usersettings">User Settings</router-link></div>
-            <div><router-link to="/" v-on:click="logout()">Logout</router-link></div>
-        </div>
+  <div class="dropdown">
+    <button class="dropbtn"><img id="profilePic2" />{{ name }} ▾</button>
+    <div class="dropmenu" id="dropmenu">
+      <div><router-link to="/usersettings">User Settings</router-link></div>
+      <div><router-link to="/" v-on:click="logout()">Logout</router-link></div>
     </div>
-    
+  </div>
 </template>
 
 <script>
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import store from '@/store/index.js';
-    export default{
-        name: 'UserDropdown',
-        data() {
-            return{
-                isOpen: false,
-                name: firebase.auth().currentUser.displayName,
-            }
-        },
-        mounted() {
-                var tempUser = firebase.auth().currentUser;
-let storageRef = firebase
-          .storage()
-          .ref(tempUser.uid + "/profilePicture/avatar.png");
-        storageRef.getDownloadURL().then(function (url) {
-          document.getElementById("profilePic2").src = url;
-        });
-        },
-        methods: {
-
-            logout() {
-                firebase.auth().signOut().then(function() {
-                    console.log("Signed out successfully.")
-                    store.state.isLoggedIn = false;
-                    window.location.reload();
-                }).catch(console.log("Signout failure!"))
-            }
-        }
-    }
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import store from "@/store/index.js";
+export default {
+  name: "UserDropdown",
+  data() {
+    return {
+      isOpen: false,
+      name: firebase.auth().currentUser.displayName,
+    };
+  },
+  mounted() {
+    var tempUser = firebase.auth().currentUser;
+    let storageRef = firebase
+      .storage()
+      .ref(tempUser.uid + "/profilePicture/avatar.png");
+    storageRef.getDownloadURL().then(function (url) {
+      document.getElementById("profilePic2").src = url;
+    });
+  },
+  methods: {
+    logout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(function () {
+          console.log("Signed out successfully.");
+          store.state.isLoggedIn = false;
+          window.location.reload();
+        })
+        .catch(console.log("Signout failure!"));
+    },
+  },
+};
 </script>
 
 <style scoped>
-
-    .dropdown{
-        float: left;
-        overflow: hidden;
-        font-size:inherit;
-    }
-
-    .dropbtn{
-        font-weight: bold;
-        font-size: inherit;
-        border: none;
-        outline: none;
-        background-color: inherit;
-        color: white;
-        margin:0;   
-    }
-
-    .dropmenu{
-        display: none;
-        position: absolute;
-        background-color: #EDEDED;
-        min-width: 120px;
-        box-shadow: 0px 2px black;
-        z-index: 1;
-        padding: 10px 0px;
-    }
-
-    .dropbtn:hover + .dropmenu{
-        display: block;
-    }
-
-    .dropmenu:hover{
-        display: block;
-    }
-
-    a{
-        color: #171717;
-
-        text-decoration: none;
-    }
-    
-    #profilePic2 {
-  height: 1.5rem;
-          padding-right: 0.5rem;
+.dropdown {
+  float: left;
+  overflow: hidden;
+  font-size: inherit;
 }
 
+.dropbtn {
+  font-weight: bold;
+  font-size: inherit;
+  border: none;
+  outline: none;
+  background-color: inherit;
+  color: white;
+  margin: 0;
+}
+
+.dropmenu {
+  display: none;
+  position: absolute;
+  background-color: #ededed;
+  min-width: 120px;
+  box-shadow: 0px 2px black;
+  z-index: 1;
+  padding: 10px 0px;
+}
+
+.dropbtn:hover + .dropmenu {
+  display: block;
+}
+
+.dropmenu:hover {
+  display: block;
+}
+
+a {
+  color: #171717;
+
+  text-decoration: none;
+}
+
+#profilePic2 {
+  height: 1.5rem;
+  padding-right: 0.5rem;
+}
+
+template {
+  display: flex;
+  align-content: right;
+  justify-content: right;
+}
 </style>
