f348738177e615ef85e18d66487a0a65159b7f62
tsoumagas-benjamin <bptsoumagas@gmail.com>
2022-04-26 17:35:19 -0400

    Added Connect 4 to home

--- src/components/HomeContainer.vue
+++ src/components/HomeContainer.vue
@@ -3,6 +3,7 @@
             <GameBox :img_src="require('../assets/home/chess2.png')" route="/chess" />
             <GameBox :img_src="require('../assets/home/ttt2.png')" route="/tictactoe" />
             <GameBox :img_src="require('../assets/home/checkers2.png')" route="/checkers" />
+            <GameBox :img_src="require('../assets/home/connect42.png')" route="/connect4" />            
         </div>
     
     
